aa96c8fe0bc50ccd29ef0320204390fd452517ab
yamagata-136 <58691624+yamagata-136@users.noreply.github.com>
2020-08-22 17:24:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/blog_getter2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.blog_getter2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
